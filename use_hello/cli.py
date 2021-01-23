aa985ebd24a2fe80c6c73dd77158c801da840aef
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-23 18:39:36 +0900

    hello world using

--- use_hello/cli.py
+++ use_hello/cli.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 import hello
+import hello.cli
 
 
 def main():
     print(hello.core.get_hmm())
+
+    print("----")
+    hello.cli.hello()
+    print("----")
 
 if __name__ == "__main__":
     main()
